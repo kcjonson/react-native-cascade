79697ad773baa83054e8f61735df309d601b78c5
Kevin Jonson <kevin.jonson@redfin.com>
2018-03-02 13:43:46 -0800

    forgot file on last commit

---
+++ src/gulp-plugin-transform-css.js
@@ -0,0 +1,29 @@
+'use strict';
+const through = require('through2');
+const PluginError = require('plugin-error');
+const stylesheetParser = require('./stylesheetParser');
+
+module.exports = options => {
+	options = options || {};
+
+	return through.obj(function (file, enc, cb) {
+		if (file.isNull()) {
+			cb(null, file);
+			return;
+		}
+
+		if (file.isStream()) {
+			cb(new PluginError('gulp-<%= pluginName %>', 'Streaming not supported'));
+			return;
+		}
+
+		try {
+			file.contents = Buffer.from(stylesheetParser(file.contents.toString(), options));
+			this.push(file);
+		} catch (err) {
+			this.emit('error', new PluginError('gulp-<%= pluginName %>', err));
+		}
+
+		cb();
+	});
+};

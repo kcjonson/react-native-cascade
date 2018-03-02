6f592332766dfbbf617078c8c0e72c19a9125fa8
Kevin Jonson <kevin.jonson@redfin.com>
2018-03-02 13:44:41 -0800

    fixing linter error

--- src/gulp-plugin-transform-css.js
+++ src/gulp-plugin-transform-css.js
@@ -1,29 +1,29 @@
-'use strict';
+/* eslint-disable no-param-reassign */
 const through = require('through2');
 const PluginError = require('plugin-error');
 const stylesheetParser = require('./stylesheetParser');
 
 module.exports = options => {
-	options = options || {};
+  options = options || {};
 
-	return through.obj(function (file, enc, cb) {
-		if (file.isNull()) {
-			cb(null, file);
-			return;
-		}
+  return through.obj(function (file, enc, cb) {
+    if (file.isNull()) {
+      cb(null, file);
+      return;
+    }
 
-		if (file.isStream()) {
-			cb(new PluginError('gulp-<%= pluginName %>', 'Streaming not supported'));
-			return;
-		}
+    if (file.isStream()) {
+      cb(new PluginError('gulp-<%= pluginName %>', 'Streaming not supported'));
+      return;
+    }
 
-		try {
-			file.contents = Buffer.from(stylesheetParser(file.contents.toString(), options));
-			this.push(file);
-		} catch (err) {
-			this.emit('error', new PluginError('gulp-<%= pluginName %>', err));
-		}
+    try {
+      file.contents = Buffer.from(stylesheetParser(file.contents.toString(), options));
+      this.push(file);
+    } catch (err) {
+      this.emit('error', new PluginError('gulp-<%= pluginName %>', err));
+    }
 
-		cb();
-	});
+    cb();
+  });
 };

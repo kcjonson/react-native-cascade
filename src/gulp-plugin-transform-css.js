01da2360ca79cf2c5328823d380272d34d802fa2
Kevin Jonson <kevin.jonson@redfin.com>
2018-03-02 15:24:16 -0800

    gulp transform for css now working

--- src/gulp-plugin-transform-css.js
+++ src/gulp-plugin-transform-css.js
@@ -6,24 +6,29 @@ const stylesheetParser = require('./stylesheetParser');
 module.exports = options => {
   options = options || {};
 
-  return through.obj(function (file, enc, cb) {
+  return through.obj(function transformCSS(file, enc, cb) {
+    // console.log('transforming css');
     if (file.isNull()) {
       cb(null, file);
       return;
     }
 
     if (file.isStream()) {
-      cb(new PluginError('gulp-<%= pluginName %>', 'Streaming not supported'));
+      cb(new PluginError('gulp-plugin-transform-css', 'Streaming not supported'));
       return;
     }
 
     try {
-      file.contents = Buffer.from(stylesheetParser(file.contents.toString(), options));
+      const transformedStylesheet = stylesheetParser(file.contents.toString(), options);
+      const moduleStylesheet = `module.exports = ${JSON.stringify(transformedStylesheet)}`;
+      file.contents = Buffer.from(moduleStylesheet);
       this.push(file);
+      cb();
     } catch (err) {
-      this.emit('error', new PluginError('gulp-<%= pluginName %>', err));
+      this.emit('error', new PluginError('gulp-plugin-transform-css', err));
+      cb();
     }
 
-    cb();
+
   });
 };

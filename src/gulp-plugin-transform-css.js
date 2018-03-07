d58fe8c8202c67d69cc96a64a290e46967c98088
Kevin Jonson <kevin.jonson@redfin.com>
2018-03-07 14:55:03 -0800

    weird error fix

--- src/gulp-plugin-transform-css.js
+++ src/gulp-plugin-transform-css.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-param-reassign */
+'use strict';
 const through = require('through2');
 const PluginError = require('plugin-error');
 const stylesheetParser = require('./stylesheetParser');
@@ -7,7 +8,6 @@ module.exports = options => {
   options = options || {};
 
   return through.obj(function transformCSS(file, enc, cb) {
-    // console.log('transforming css');
     if (file.isNull()) {
       cb(null, file);
       return;
@@ -23,12 +23,15 @@ module.exports = options => {
       const moduleStylesheet = `module.exports = ${JSON.stringify(transformedStylesheet)}`;
       file.contents = Buffer.from(moduleStylesheet);
       this.push(file);
-      cb();
     } catch (err) {
-      this.emit('error', new PluginError('gulp-plugin-transform-css', err));
-      cb();
+      // Something weird is happening with errors. Need to figure that out... later.
+      console.log(err);
+      cb(new PluginError('gulp-plugin-transform-css', err));
+      // this.emit('error', new PluginError('gulp-plugin-transform-css', err));
+      // this.emit('error', err);
+      // cb();
     }
-
+    cb();
 
   });
 };
